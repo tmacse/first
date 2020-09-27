f6ec1e8d6b04673ccff3682d84b2e5fe20774184
wangbing <tmacse@163.com>
2020-09-27 17:45:32 +0800

    解决了页面跳转无法回到页面顶部的bug,使用了ScrollToTop.js组件

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import List from './components/list/List';
 import Detail from './components/detail/Detail';
 import MovieDetail from './components/movieDetail/MovieDetail'
+import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <Provider store={store}>
       <div>
         <BrowserRouter>
-          <Route path="/" component={Home} exact />
-          <Route path='/list' component={List} exact />
-          <Route path='/detail/:_id' component={Detail} exact />
-          <Route path='/movie_detail' component={MovieDetail} exact />
+          <ScrollToTop>
+            <Route path="/" component={Home} exact />
+            <Route path='/list' component={List} exact />
+            <Route path='/detail/:_id' component={Detail} exact />
+            <Route path='/movie_detail' component={MovieDetail} exact />
+          </ScrollToTop>
         </BrowserRouter>
       </div>
     </Provider>
