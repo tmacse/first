16f4fd49a9f701730b06fafa0f21f322c3e95ba6
wangbing <tmacse@163.com>
2020-10-19 15:43:14 +0800

    axios传值

--- src/components/list/store/actionCreators.js
+++ src/components/list/store/actionCreators.js
@@ -4,7 +4,7 @@ import { GET_NOTICE_LIST } from './actionTypes';
 //异步获得通知列表的界面
 export const getList = (attr, page) => {
 	return (dispatch) => {
-		axios.get(`/${attr}/list?pageNum=` + page).then((res) => {
+		axios.get(`/${attr}/list`, { params: { 'attr': attr, 'pageNum': page, 'pageSize': 10 } }).then((res) => {
 			const result = res.data.data;
 			console.log('1', result)
 			const action = {
