3d48ac14968f2d645b51b30770e9174dbd081efb
wangbing <tmacse@163.com>
2020-11-02 09:17:39 +0800

    添加了主页到邮件分页面的链接

--- src/components/content/qjScale/QjMail.js
+++ src/components/content/qjScale/QjMail.js
@@ -6,29 +6,37 @@ import { Link } from 'react-router-dom';
 export default class QjMail extends Component {
     render() {
         return (
-            <div>
+            <>
                 <Row>
                     <Col className="station-master-mail" span={6}>
-                        <MailOutlined />
                         <Link to='/mail/zz'>
+                            <MailOutlined />
                             <span className="mail-text">站长信箱</span>
                         </Link>
 
                     </Col>
                     <Col className="commissioner-mail" span={6}>
-                        <MailOutlined />
-                        <span className="mail-text">政委信箱</span>
+                        <Link to='/mail/zw'>
+                            <MailOutlined />
+                            <span className="mail-text">政委信箱</span>
+                        </Link>
                     </Col>
                     <Col className="officer-mail" span={6}>
-                        <MailOutlined />
-                        <span className="mail-text">主任信箱</span>
+
+                        <Link to='/mail/zr'>
+                            <MailOutlined />
+                            <span className="mail-text">主任信箱</span>
+                        </Link>
+
                     </Col>
                     <Col className="discipline-mail" span={6}>
-                        <MailOutlined />
-                        <span className="mail-text">纪委信箱</span>
+                        <Link to='/mail/jw'>
+                            <MailOutlined />
+                            <span className="mail-text">纪委信箱</span>
+                        </Link>
                     </Col>
                 </Row>
-            </div>
+            </>
         )
     }
 }
