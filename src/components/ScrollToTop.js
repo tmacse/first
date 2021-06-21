9e91d73200ec5d0383e63db62e256f569e83b5eb
wangbing <tmacse@163.com>
2021-06-21 22:14:19 +0800

    修复了bug

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,25 +1,11 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
-class ScrollToTop extends Component {
-    componentDidUpdate(prevProps) {
-        if (this.props.location !== prevProps.location) {
-            window.scrollTo(0, 0)
-        }
-    }
-    render() {
-        return this.props.children
-    }
+const ScrollToTop = (props) => {
+    const { location, children } = props
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [location])
+    return children
 }
-
-// function ScrollToTop(props) {
-//     console.log(props)
-//     useEffect((prevProps) => {
-//         if (props.location !== prevProps.location) {
-//             window.scrollTo(0, 0)
-//         }
-
-//     }, [props.location])
-//     return props.children
-// }
 export default withRouter(ScrollToTop);
